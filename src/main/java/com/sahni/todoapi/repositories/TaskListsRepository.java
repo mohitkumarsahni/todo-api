9e1dc8cc7c8405ada4d71f38d8b062b1df2c9fa4
Mohit Kumar Sahni <mohitkumarsahni@gmail.com>
2022-12-26 03:05:22 +0530

    added repositories for entity classes

---
+++ src/main/java/com/sahni/todoapi/repositories/TaskListsRepository.java
@@ -0,0 +1,11 @@
+package com.sahni.todoapi.repositories;
+
+import com.sahni.todoapi.models.TaskLists;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListsRepository extends JpaRepository<TaskLists, UUID> {
+}
---
+++ src/main/java/com/sahni/todoapi/repositories/TasksRepository.java
@@ -0,0 +1,11 @@
+package com.sahni.todoapi.repositories;
+
+import com.sahni.todoapi.models.Tasks;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Tasks, UUID> {
+}
